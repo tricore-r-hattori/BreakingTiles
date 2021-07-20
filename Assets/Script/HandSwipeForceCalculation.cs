68164437d9b5d44472dd12f1a2c0ae08b76bfedd
reooo <tricore.as09@gmail.com>
2021-07-20 11:05:02 +0900

    スワイプ時の力の計算PR修正

--- Assets/Script/HandSwipeForceCalculation.cs
+++ Assets/Script/HandSwipeForceCalculation.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
+/// <summary>
+/// スワイプ時の力の計算
+/// </summary>
 public class HandSwipeForceCalculation : MonoBehaviour
 {
     // 手
@@ -14,32 +17,42 @@ public class HandSwipeForceCalculation : MonoBehaviour
     [SerializeField]
     RectTransform tileTransform = default;
 
+    // 座標を取得するタイミング(フレーム)
+    [SerializeField]
+    int getPositionTime = 60;
+
     // 手の座標
-    Vector3 handPos = new Vector3(0.0f,0.0f,0.0f);
+    Vector3 handPos = Vector3.zero;
     // 瓦の座標
-    Vector3 tilePos = new Vector3(0.0f,0.0f,0.0f);
+    Vector3 tilePos = Vector3.zero;
 
     // スワイプした距離
     float distance = 0.0f;
     // 速度
     float speed = 0.0f;
+    // 秒
+    float seconds = 0.0f;
+    // スワイプ時の力
+    // TODO: 瓦との当たり判定をとった後に使用します。
+    float swipeForce = 0.0f;
     // フレームのカウント
     int frameCount = 0;
-
-    // 座標を取得するタイミング(フレーム)
-    const int getPositionTime = 60;
 
     // 手の座標の補正値
     const float correctionHandPositionsY = 1.3f;
     // 瓦の座標の補正値
     const float correctionTilePositionsY = 0.7f;
+    // フレームから秒に変える値
+    const float frameToSeconds = 60.0f;
 
     /// <summary>
     /// 初期化処理
     /// </summary>
     void Start()
     {
+        // 瓦の座標を仮に設定
         tilePos = tileTransform.position;
+        // 瓦のY軸補正
         tilePos.y += correctionTilePositionsY;
     }
 
@@ -51,26 +64,35 @@ public class HandSwipeForceCalculation : MonoBehaviour
         frameCount++;
 
         // 1秒ごとに手の座標を取得する
-        if (frameCount % 60.0f == 0)
+        if (frameCount % getPositionTime == 0)
         {
+            // 手の座標を取得
             handPos = handTransform.position;
+            // 手のY軸補正
             handPos.y -= correctionHandPositionsY;
 
+            // フレームのカウントを初期化
             frameCount = 0;
         }
 
         // マウスの左ボタンを離した時
-        // 瓦との当たり判定を取っていないので、ここの条件式は一旦仮でマウスの左ボタンを離した時に処理を行うようにしています。
-        // 本来は瓦と当たった時のフラグを条件式にします。
+        // NOTE: 瓦との当たり判定を取っていないので、ここの条件式は一旦仮でマウスの左ボタンを離した時に処理を行うようにしています。
+        //       本来は瓦と当たった時のフラグを条件式にします。
         if (Input.GetMouseButtonUp(0))
         {
-            // スワイプした距離を取得する
-            distance = Mathf.Abs(Vector3.Distance(handPos, tilePos));
+            // スワイプした距離を計算
+            distance = Mathf.Abs(Vector3.SqrMagnitude(handPos - tilePos));
+
+            // フレームから秒の値に変換
+            seconds = (getPositionTime / frameToSeconds);
+
+            // 速度を計算
+            speed = distance / seconds * seconds;
         }
     }
 
     // 瓦の当たる位置によって割る力を変化させる処理
-    // 瓦との当たり判定をとっていないので瓦を割るタスクを行う際にここの処理を書きます。
+    // TODO: 瓦との当たり判定をとっていないので瓦を割るタスクを行う際にここの処理を書きます。
     // public void TileIsHitPower()
     // {
 
--- Assets/Script/PositionSynchronizeObject.cs
+++ Assets/Script/PositionSynchronizeObject.cs
@@ -6,40 +6,43 @@ using System.Collections;
 /// </summary>
 public class PositionSynchronizeObject : MonoBehaviour
 {
-	// マウスのスクリーン座標
-	Vector3 mouseScreenPosition = new Vector3(0.0f, 0.0f, 0.0f);
-	// スクリーン座標をワールド座標に変換したマウスの座標
-	Vector3 screenToWorldMousePosition = new Vector3(0.0f, 0.0f, 0.0f);
-	// マウスを左クリックした際にプレイヤーと当たったかどうかのフラグ
-	bool playerToHit = false;
-
-	/// <summary>
-	/// 更新処理
-	/// </summary>
-	void Update()
-	{
-		// マウスの左ボタンで押した時
-		if (Input.GetMouseButtonDown(0))
-		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			playerToHit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
-		}
-
-		// 対象のオブジェクトと当たったか
-		if (playerToHit)
-		{
-			// マウスの左ボタンで離した時
-			if (Input.GetMouseButton(0))
-			{
-				// マウスの座標を取得する
-				mouseScreenPosition = Input.mousePosition;
-				// Z軸修正
-				mouseScreenPosition.z = 10f;
-				// マウスの座標をスクリーン座標からワールド座標に変換する
-				screenToWorldMousePosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
-				// ワールド座標に変換されたマウスの座標をアタッチされているオブジェクトの座標に代入
-				gameObject.transform.position = screenToWorldMousePosition;
-			}
-		}
-	}
+    // マウスのスクリーン座標
+    Vector3 mouseScreenPosition = Vector3.zero;
+    // スクリーン座標をワールド座標に変換したマウスの座標
+    Vector3 screenToWorldMousePosition = Vector3.zero;
+    // マウスを左クリックした際にプレイヤーと当たったかどうかのフラグ
+    bool playerToHit = false;
+    
+    // マウスの座標の補正値
+    const float correctionHandPositionsY = 10.0f;
+    
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        // マウスの左ボタンで押した時
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            playerToHit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+        }
+        
+        // 対象のオブジェクトと当たったか
+        if (playerToHit)
+        {
+            // マウスの左ボタンで離した時
+            if (Input.GetMouseButton(0))
+            {
+                // マウスの座標を取得する
+                mouseScreenPosition = Input.mousePosition;
+                // マウスのZ軸補正
+                mouseScreenPosition.z = correctionHandPositionsY;
+                // マウスの座標をスクリーン座標からワールド座標に変換する
+                screenToWorldMousePosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+                // ワールド座標に変換されたマウスの座標をアタッチされているオブジェクトの座標に代入
+                gameObject.transform.position = screenToWorldMousePosition;
+            }
+        }
+    }
 }
