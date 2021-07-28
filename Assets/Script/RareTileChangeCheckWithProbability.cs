3fc13504d496d08f6c4060617e96d46c24dd915a
reooo <tricore.as09@gmail.com>
2021-07-28 17:59:00 +0900

    低確率でレア瓦が出現する処理を実装

---
+++ Assets/Script/RareTileChangeCheckWithProbability.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 確率判定でレア瓦の画像を変更するか確認するためのクラス
+/// </summary>
+public class RareTileChangeCheckWithProbability : MonoBehaviour
+{
+    // 確率
+    [SerializeField]
+    float percent = 100.0f;
+
+    /// <summary>
+    /// レア瓦の画像を変更するかのフラグ
+    /// </summary>
+    public bool IsRareTileChange { get; private set; } = false;
+    
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    void Start()
+    {
+        // レア瓦の画像を変更するかのフラグに確率判定結果を保存する
+        IsRareTileChange = Probability_(percent);
+    }
+
+    /// <summary>
+    /// 確率判定
+    /// </summary>
+    /// <param name="_percent">確率 (0～100)</param>
+    /// <returns>当選結果 [true]当選,[false]落選</returns>
+    bool Probability_(float _percent)
+    {
+        // 乱数を計算
+        float probabilityRate = Random.value * 100.0f;
+
+        // 確率が100%かつ確率が乱数の値と一緒だったら当選する
+        if (_percent == 100.0f && probabilityRate == _percent)
+        {
+            return true;
+        }
+        // 乱数の値が確率の値より小さかったら当選する
+        else if (probabilityRate < _percent)
+        {
+            return true;
+        }
+        // 上記の条件以外だったら落選
+        else
+        {
+            return false;
+        }
+    }
+}
--- Assets/Script/TileImageChanger.cs
+++ Assets/Script/TileImageChanger.cs
@@ -14,14 +14,11 @@ public class TileImageChanger : MonoBehaviour
 
     // 瓦
     [SerializeField]
-    RectTransform imageTransform = default;
+    RectTransform tileTransform = default;
 
     // 瓦の画像を変える地点
     [SerializeField]
     RectTransform tileSpriteChangePoint = default;
-
-    [SerializeField]
-    Probability probability;
 
     // 割れていない瓦の画像
     [SerializeField]
@@ -39,32 +36,59 @@ public class TileImageChanger : MonoBehaviour
     [SerializeField]
     Sprite breakRareTileSprite = default;
 
+    // 確率判定でレア瓦の画像を変更するか確認
+    [SerializeField]
+    RareTileChangeCheckWithProbability rareTileChangeCheck = default;
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    void Start()
+    {
+        // レア瓦の画像を変更できる状態ならレア瓦の画像に設定
+        if (rareTileChangeCheck.IsRareTileChange)
+        {
+            // レア瓦の画像に設定
+            tileImage.sprite = rareTileSprite;
+        }
+        // レア瓦の画像を変更できない状態なら通常の瓦の画像に設定
+        else
+        {
+            // 通常の瓦の画像に設定
+            tileImage.sprite = tileSprite;
+        }
+    }
+
     /// <summary>
     /// 更新処理
     /// </summary>
     void Update()
     {
-        // 割れていない状態に戻す地点に到達したら画像を変換
-        if (imageTransform.position.y < tileSpriteChangePoint.position.y && tileImage.sprite == breakTileSprite)
+        // 画像を変える地点より低い位置にいた時かつ割れている瓦の状態かつレア瓦に変更できない時に画像を変換
+        if (tileTransform.position.y < tileSpriteChangePoint.position.y && tileImage.sprite == breakTileSprite && !rareTileChangeCheck.IsRareTileChange)
         {
             // 割れていない瓦に変換
             tileImage.sprite = tileSprite;
         }
 
-        // 割れている状態に戻す地点に到達したら画像を変換
-        if (imageTransform.position.y > tileSpriteChangePoint.position.y && tileImage.sprite == tileSprite)
+        // 画像を変える地点より高い位置にいた時かつ割れていない瓦の状態かつレア瓦に変更できない時に画像を変換
+        if (tileTransform.position.y > tileSpriteChangePoint.position.y && tileImage.sprite == tileSprite && !rareTileChangeCheck.IsRareTileChange)
         {
             // 割れている瓦に変換
             tileImage.sprite = breakTileSprite;
         }
 
-        if (probability.IsRareTile && imageTransform.position.y < tileSpriteChangePoint.position.y /*&& tileImage.sprite == breakRareTileSprite*/)
+        // 画像を変える地点より低い位置にいた時かつ割れているレア瓦の状態かつレア瓦に変更できる時に画像を変換
+        if (tileTransform.position.y < tileSpriteChangePoint.position.y && tileImage.sprite == breakRareTileSprite && rareTileChangeCheck.IsRareTileChange)
         {
+            // 割れていないレア瓦に変換
             tileImage.sprite = rareTileSprite;
         }
 
-        if (probability.IsRareTile && imageTransform.position.y > tileSpriteChangePoint.position.y /*&& tileImage.sprite == rareTileSprite*/)
+        // 画像を変える地点より高い位置にいた時かつ割れていないレア瓦の状態かつレア瓦に変更できる時に画像を変換
+        if (tileTransform.position.y > tileSpriteChangePoint.position.y && tileImage.sprite == rareTileSprite && rareTileChangeCheck.IsRareTileChange)
         {
+            // 割れているレア瓦に変換
             tileImage.sprite = breakRareTileSprite;
         }
     }
