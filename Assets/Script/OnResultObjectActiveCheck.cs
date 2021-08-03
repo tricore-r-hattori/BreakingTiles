8b9149e3c916a54129f185431ce9a3a953b986c3
reooo <tricore.as09@gmail.com>
2021-08-03 12:55:30 +0900

    OnResultObjectActiveCheck.cs、SoundController.cs追加

---
+++ Assets/Script/OnResultObjectActiveCheck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// リザルトオブジェクトがアクティブになったか確認
+/// </summary>
+public class OnResultObjectActiveCheck : MonoBehaviour
+{
+    // リザルトオブジェクトがアクティブになった時に効果音を流すためのAction
+    event Action<AudioClip> onPlayResultSound;
+
+    // リザルト遷移時の音
+    [SerializeField]
+    AudioClip resultAudioClip = default;
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    /// <param name="_onPlayResultSound">// リザルトオブジェクトがアクティブになった時に効果音を流すためのAction</param>
+    public void Init(Action<AudioClip> _onPlayResultSound)
+    {
+        this.onPlayResultSound = _onPlayResultSound;
+    }
+
+    /// <summary>
+    /// アクティブ化した時に1回だけ処理を行う
+    /// </summary>
+    void OnEnable()
+    {
+        // リザルトオブジェクトがアクティブになった時に効果音を流すためのAction
+        onPlayResultSound(resultAudioClip);
+    }
+}
---
+++ Assets/Script/SoundController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 音を操作する
+/// </summary>
+public class SoundController : MonoBehaviour
+{
+    // 瓦の画像を変えるクラス
+    [SerializeField]
+    List<TileImageChanger> tileImageChanger = default;
+
+    // リザルトオブジェクトがアクティブになったか確認
+    [SerializeField]
+    OnResultObjectActiveCheck onResultObjectActiveCheck = default;
+
+    // オーディオソース
+    [SerializeField]
+    AudioSource audioSource = default;
+
+    /// <summary>
+    /// 開始処理
+    /// </summary>
+    void Start()
+    {
+        for (int i = 0; i < tileImageChanger.Count; i++)
+        {
+            tileImageChanger[i].InitPlayBreakTileSound(PlaySound);
+        }
+
+        onResultObjectActiveCheck.Init(PlaySound);
+    }
+
+    /// <summary>
+    /// 音を流す
+    /// </summary>
+    /// <param name="_audioClip">音の素材</param>
+    void PlaySound(AudioClip _audioClip)
+    {
+        audioSource.PlayOneShot(_audioClip);
+    }
+}
